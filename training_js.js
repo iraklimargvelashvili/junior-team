568b09a3c6f14eb2d4a6a975ec81236f1f070972
iraklimargvelashvili <ecseminari@gmail.com>
2025-03-23 14:25:01 +0400

    second_person_first_commit

--- training_js.js
+++ training_js.js
@@ -3,3 +3,7 @@ console.log("We are the Titans of programming!!!")
 console.log("We can do this!")
 
 console.log("This was interesting")
+
+console.log(" - - - - - - - - - - - - - ")
+
+console.log("I try and I do it")
